fafe70355a3462049e4abcadc2c5cc95910372bf
prawnzyy <jamcookies77@gmail.com>
2024-09-17 11:26:48 -0400

    Add question 3

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def main(res_data, country_data):
     # Prepare DF to write to CSV
     zomato = pd.json_normalize(events["restaurant.zomato_events"])
     photo_urls = zomato["event.photos"]
-    photo_urls = photo_urls.apply(lambda x: x[0]['photo']['url'] if x != [] else "Na")
+    photo_urls = photo_urls.apply(lambda x: x[0]['photo']['url'] if x != [] else "NA")
     zomato = zomato.assign(photo_urls=photo_urls.values)
     events = pd.concat([events.drop(columns="restaurant.zomato_events"), zomato], axis = 1)
 
@@ -50,6 +50,38 @@ def main(res_data, country_data):
                        "photo_urls", "event.title", "event.start_date", "event.end_date"]].copy()
     to_write.to_csv("restaurant_events.csv", index=False)
 
+    # Question 3
+
+    """
+    Assumptions made:
+    1. The data where the country Ids do not much are not being used
+    2. Restaurants which have fake reviews should not be counted into the threshold value to prevent
+    skewing of data
+    3. The Threshold of the ratings is determined by the lowest value of that particular rating
+    """
+    # Obtain columns that have user rating information
+    user_ratings = res_df.filter(regex="user_rating")
+
+    # Remove those restaurants with fake reviews
+    mask = user_ratings["restaurant.user_rating.has_fake_reviews"].apply(lambda x: True if x == 0 else False)
+    user_ratings = user_ratings[mask]
+
+    # Store the minimum value of each rating in a dictionary
+    rating_threshold = {}
+    for index, row in user_ratings.iterrows():
+        rating = row["restaurant.user_rating.aggregate_rating"]
+        text = row["restaurant.user_rating.rating_text"]
+        if text not in rating_threshold:
+            rating_threshold[text] = rating
+        else:
+            rating_threshold[text] = min(rating, rating_threshold[text])
+    
+    # Display required ratings in a table.
+    required_rating = ["Excellent", "Very Good", "Good", "Average", "Poor"]
+    print(f"{"Threshold": <12} {"Value"}")
+    for rating in required_rating:
+        print(f"{rating + ":": <12} {rating_threshold[rating]}")
+    
 if __name__ == "__main__":
     country_data = Excel_Reader("https://github.com/Papagoat/brain-assessment/blob/main/Country-Code.xlsx?raw=true")
     restaurant_data = Json_Reader("https://raw.githubusercontent.com/Papagoat/brain-assessment/main/restaurant_data.json")
